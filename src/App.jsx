fed036af7d9e23d01ae1be46e5e2b3af21845a2e
joaopamarall <joaoamaral56@hotmail.com>
2023-12-15 13:45:05 -0300

    refactor: refatorar código para melhor leitura de lógica.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0);
-  
-  function decrementBtn() {
-    setCount((count) => count - 1)
-  }
-  function incrementBtn() {
-    setCount((count) => count + 1);
-  }
+  const decrementar = () => setCount((count) => count - 1);
+  const incrementar = () => setCount((count) => count + 1);
 
   useEffect(() => {
     setCount(10);
@@ -20,12 +17,11 @@ function App() {
       <h1>Teste de Estágio em React</h1>
       <h3>Questão 2</h3>
       <div className="card">
-        <button onClick={decrementBtn}>-1</button>
-        <button onClick={incrementBtn}>+1</button>
+        <button onClick={decrementar}>-1</button>
+        <button onClick={incrementar}>+1</button>
         <div className="contador">{count}</div>
       </div>
     </>
   )
 }
-
 export default App
